de8c11aaf8da379352f723ccaca62e95a27f45d6
Hilal <72453298+Hilal-Almoqbali@users.noreply.github.com>
2022-12-25 10:17:17 +0400

    add

---
+++ src/main.cc
@@ -0,0 +1,71 @@
+#include <GLFW/glfw3.h>
+
+// Define the objects and assets in your game
+struct Player {
+  glm::vec3 position;
+  glm::vec3 velocity;
+  float rotation;
+};
+struct Cube {
+  glm::vec3 position;
+  glm::vec3 velocity;
+  float size;
+};
+
+// Initialize the player and the objects in your game
+Player player;
+Cube cubes[10];
+
+// Set up the window and the OpenGL context
+GLFWwindow* window;
+int width = 800, height = 600;
+
+void init() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  window = glfwCreateWindow(width, height, "3D Game", NULL, NULL);
+  glfwMakeContextCurrent(window);
+
+  // Initialize the player and the objects in your game
+  player.position = glm::vec3(0, 0, 0);
+  player.velocity = glm::vec3(0, 0, 0);
+  player.rotation = 0.0f;
+  for (int i = 0; i < 10; i++) {
+    cubes[i].position = glm::vec3(i * 2.0f, 0, 0);
+    cubes[i].velocity = glm::vec3(0, 0, 0);
+    cubes[i].size = 1.0f;
+  }
+}
+
+// Update the game state
+void update(float dt) {
+  // Process input
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    player.velocity.z -= 1.0f;
+  }
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    player.velocity.z += 1.0f;
+  }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    player.velocity.x -= 1.0f;
+  }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    player.velocity.x += 1.0f;
+  }
+
+  // Update the player and the objects in your game
+  player.position += player.velocity * dt;
+  player.velocity *= 0.9f;
+  player.rotation += 90.0f * dt;
+  for (int i = 0; i < 10; i++) {
+    cubes[i].position += cubes[i].velocity * dt;
+    cubes[i].velocity *= 0.9f;
+  }
+}
+
+// Render the game
+void render() {
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT |
